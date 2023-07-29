21522ef4d518dbda7c424ee5505d2b4534e0740a
Naveed <naveedhassankhan512@gamil.com>
2023-07-29 18:39:30 +0500

    Commit it

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function removeClass(e) {
     if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('play');
+    this.classList.remove('play');
   }
-
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key = "${e.keyCode}"]`);
-    
-    if (!audio) return
+  
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`div[data-key="${e.code}"]`);
+  
+    if (!audio) return;
     key.classList.add('play');
     audio.currentTime = 0;
     audio.play();
-}
-
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeClass));
-
-window.addEventListener("keydown", playSound);
+  }
+  
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener("transitionend", removeClass));
+  window.addEventListener("keydown", playSound);
+  
